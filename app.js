34de7fd2666637dd69dba7f6d6b664b179d700c0
Andrew Braithwaite <abraithw@gmail.com>
2014-12-01 20:02:38 +0000

    use promises

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 #!/usr/bin/env node
 
+var Q = require('q')
 var exec = require('child_process').exec;
 var fs = require('fs');
 
-exec('echo \'4444333322221111\' | openssl rsautl -encrypt -inkey public.pem -pubin | base64', function(err, stdout, stderr) {
-  console.info('encrypted string:', stdout)
-  exec('echo "' + stdout +'" | base64 --decode | openssl rsautl -decrypt -inkey private.pem', function(err, stdout, stderr) {
-    console.info('decrypted string:', stdout);
+var encrypt = function(txt) {
+  var deferred = Q.defer();
+
+  exec('echo "' + txt + '" | openssl rsautl -encrypt -inkey public.pem -pubin | base64', function(err, stdout, stderr) {
+    if (err) {
+      deferred.reject('Encryption failed...')
+    } else {
+      deferred.resolve(stdout);
+    }
   });
+
+  return deferred.promise;
+}
+
+var decrypt = function(txt) {
+  var deferred = Q.defer();
+
+  exec('echo "' + txt + '" | base64 --decode | openssl rsautl -decrypt -inkey private.pem', function(err, stdout, stderr) {
+    if (err) {
+      deferred.reject('Decryption failed...')
+    } else {
+      deferred.resolve(stdout);
+    }
+  });
+
+  return deferred.promise;
+}
+
+encrypt('4444333322221111').then(function(encrypted_txt) {
+  return decrypt(encrypted_txt)
+}).then(function(decrypted_txt) {
+  console.info(decrypted_txt)
 });
