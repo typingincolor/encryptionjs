0a01012f031a3e95caee48209593df92b75a72f3
Andrew Braithwaite <abraithw@gmail.com>
2014-12-01 18:43:11 +0000

    do not use files

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 var exec = require('child_process').exec;
 var fs = require('fs');
 
-exec('echo \'4444333322221111\' | openssl rsautl -encrypt -inkey public.pem -pubin -out safe.txt', function(err, stdout, stderr) {
-  exec('openssl rsautl -decrypt -inkey private.pem -in safe.txt', function(err, x, stderr) {
-    console.info(x);
+exec('echo \'4444333322221111\' | openssl rsautl -encrypt -inkey public.pem -pubin | base64', function(err, stdout, stderr) {
+  console.info('encrypted string:', stdout)
+  exec('echo "' + stdout +'" | base64 --decode | openssl rsautl -decrypt -inkey private.pem', function(err, stdout, stderr) {
+    console.info('decrypted string:', stdout);
   });
 });
