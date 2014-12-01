5c780d8e5a87cbf81e9ffb62ee4d96c28566e10c
Andrew Braithwaite <abraithw@gmail.com>
2014-12-01 21:06:27 +0000

    jslint fixes

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 #!/usr/bin/env node
 
-var Q = require('q')
+var Q = require('q');
 var exec = require('child_process').exec;
 var fs = require('fs');
 
-var encrypt = function(txt) {
+var encrypt = function (txt) {
   var deferred = Q.defer();
 
-  exec('echo "' + txt + '" | openssl rsautl -encrypt -inkey public.pem -pubin | base64', function(err, stdout, stderr) {
+  exec('echo "' + txt + '" | openssl rsautl -encrypt -inkey public.pem -pubin | base64', function (err, stdout, stderr) {
     if (err) {
-      deferred.reject('Encryption failed...')
+      deferred.reject(stderr);
     } else {
       deferred.resolve(stdout);
     }
   });
 
   return deferred.promise;
-}
+};
 
-var decrypt = function(txt) {
+var decrypt = function (txt) {
   var deferred = Q.defer();
 
-  exec('echo "' + txt + '" | base64 --decode | openssl rsautl -decrypt -inkey private.pem', function(err, stdout, stderr) {
+  exec('echo "' + txt + '" | base64 --decode | openssl rsautl -decrypt -inkey private.pem', function (err, stdout, stderr) {
     if (err) {
-      deferred.reject('Decryption failed...')
+      deferred.reject(stderr);
     } else {
       deferred.resolve(stdout);
     }
   });
 
   return deferred.promise;
-}
+};
 
-encrypt('4444333322221111').then(function(encrypted_txt) {
-  return decrypt(encrypted_txt)
-}).then(function(decrypted_txt) {
-  console.info(decrypted_txt)
+encrypt('4444333322221111').then(function (encrypted_txt) {
+  return decrypt(encrypted_txt);
+}).then(function (decrypted_txt) {
+  console.info(decrypted_txt);
 });
